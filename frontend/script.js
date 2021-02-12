7199a9c7738ceb01aaabf79f7d7bf4ac549748e2
Rezan92 <rezanibrahim8@gmail.com>
2021-02-12 20:39:46 +0100

    Flip cards functionality

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,22 @@
 const app = () => {
   const URL = "http://localhost:5000/random/numbers";
+
   const selectCardsContainer = document.querySelector("#select-cards_container");
   const selectCards = document.querySelector("#select-cards_num");
   const startBtn = document.querySelector("#start-btn");
   const cardsContainer = document.querySelector("#cards-container");
   const cards = document.querySelector("#cards");
+  const card = document.getElementsByClassName("mycard");
   const playBtn = document.querySelector("#play-btn");
   const moreCardsBtn = document.querySelector("#more-cards");
+
+  const selectedCardsByUser = [];
+  const allCards = [];
 
   selectCards.addEventListener("change", validateBtn);
   startBtn.addEventListener("click", displayData);
   moreCardsBtn.addEventListener("click", goBack);
+  playBtn.addEventListener("click", flipCards);
 
   //If the value is not a valid number the button will be disabled
   function validateBtn() {
@@ -38,11 +44,18 @@ const app = () => {
     });
     const content = await response.json();
 
-    content.forEach((element) => {
+    content.forEach((num) => {
+      allCards.push(num);
+
       cards.innerHTML += `
       <div class="col-6 col-sm-3 my-2">
-        <div class="col-12 p-1 border border-primary">${element}</div>
-      </div>`;
+            <div class="col-10 mx-auto mycard-container">
+              <div class="mycard" data-number=${num}>
+                <h3 class="front">${num}</h3>
+                <div class="back">back</div>
+              </div>
+            </div>
+          </div>`;
     });
 
     addClass(selectCardsContainer, "d-none");
@@ -56,6 +69,21 @@ const app = () => {
     addClass(selectCardsContainer, "d-block");
     addClass(cardsContainer, "d-none");
     removeClass(cardsContainer, "d-block");
+  }
+
+  function flipCards() {
+    selectedCardsByUser.length = 0;
+    [...card].forEach((ele) => {
+      ele.addEventListener("click", flipCardBack);
+      addClass(ele, "rotate");
+    });
+  }
+
+  function flipCardBack(e) {
+    const element = e.target.parentElement;
+    const number = parseInt(element.getAttribute("data-number"));
+    removeClass(element, "rotate");
+    selectedCardsByUser.push(number);
   }
 
   function addClass(ele, className) {
